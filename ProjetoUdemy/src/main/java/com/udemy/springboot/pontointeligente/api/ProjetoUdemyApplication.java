d88d08dff4b2ce0ab8cedd6e7ea9783b646f1e06
Carlos Sempe <sempejunior@gmail.com>
2019-01-27 16:50:22 -0200

    Commit inicial do projeto

---
+++ ProjetoUdemy/src/main/java/com/udemy/springboot/pontointeligente/api/ProjetoUdemyApplication.java
@@ -0,0 +1,14 @@
+package com.udemy.springboot.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUdemyApplication.class, args);
+	}
+
+}
+
